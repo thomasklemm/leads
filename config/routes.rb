bf861a8c187e84905aa3073bbf5bf0b2f9c06ca5
Thomas Klemm <github@tklemm.eu>
2013-07-05 14:41:20 +0200

    Root to simyo

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Leads::Application.routes.draw do
   # Authors
   resources :authors, only: [:show, :update]
 
-  root 'authors#index'
+  root 'authors#show', id: 'simyo'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
